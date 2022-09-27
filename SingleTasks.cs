3881548fd919726e2178e19cd0b9d7f5e19f7fe4
jotades <you@example.com>
2022-09-27 23:45:12 +0200

    Updated with Notes

--- SingleTasks.cs
+++ SingleTasks.cs
@@ -1,1 +1,53 @@
 using System;
+// Test Singoli:
+// 1-Reazione visivo-spaziale 27 LAMPADE 
+// Get: Errori
+// Get: Corrette
+// Get: Tempo di reazione: stimolo – rilascio acceleratore (tempo di reazione), 
+// Get: Tempo di reazione: rilascio acceleratore-pressione freno (tempo di azione), 
+// Get: Somma dei due;
+// Set: Stimoli ogni 3-5 Secondi
+
+// 2-Reazione visivo-cognitiva 
+
+// Get: Errori
+// Get: Corrette
+// Get: Misurazione tempo di reazione (stimolo verde-acceleratore- stimolo rosso-freno)
+// Set: Velocità media da mantenere
+// Enum: 1) 1 singolo semaforo o 2 dello stesso colore
+//              2) 3 semafori totali di cui 2 dello stesso colore. 
+             
+// 3-Vista periferica e reazione acustica 
+
+// Get: Errori
+// Get: Corrette
+// Get: Tempi di reazione (stimolo-azione)
+// Set: Numero elementi
+// Set: Frequenza
+// Set: Posizione
+// Set: Durata Task
+// Set: Apparizione oggetto
+// Set: Colore
+
+// 4-Capacità modulazione del comando dell’acceleratore 
+
+// Set: Velocità Target da seguire(variabile nel tempo)
+// Get: Delta Velocità
+// Get: Tempo ripristino velocità target
+
+// 5-Capacità di rotazione del volante 
+
+// Set: n. ripetizione Task
+// Get: Velicità rotazione (rpm)
+// Get: Max angolazione raggiunta
+
+// 6-Misurazione forza esercitata sul volante 
+
+// Set: n. sec. durata Task 
+// Get:Forza sul volante
+// Get: Max angolazione raggiunta
+// 7-Misurazione della forza esercitata sul comando del freno 
+
+// Set: n. sec. durata Task 
+// Get: Forza sul pedale freno
+// Get: Tempo pedale schiacciato
